4dba3373a026c63a046b38e48d144425a78e91ef
Don Jackson <don.jackson@microfocus.com>
2021-01-22 18:55:47 +0000

    Updated with test code, initial version with completed test.

--- src/main/java/com/mf/UFTDeveloperTest.java
+++ src/main/java/com/mf/UFTDeveloperTest.java
@@ -1,5 +1,8 @@
 package com.mf;
 
+import com.hp.lft.report.ReportException;
+import com.hp.lft.report.Reporter;
+import com.hp.lft.report.Status;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -41,11 +44,56 @@ public class UFTDeveloperTest extends UnitTestClassBase {
 
     @After
     public void tearDown() throws Exception {
+        browser.closeAllTabs();
     }
 
     @Test
     public void test() throws GeneralLeanFtException {
+        //Reporter.Init(new ReportConfiguration());// Initialize the Reporter with default values
+        /*try {
+            Reporter.init();
+        } catch (ReportException e) {
+            e.printStackTrace();
+        }
+
+         */
         browser.navigate("http://nimbusserver.aos.com:8087");
+        browser.sync();
+        WebElement labelLOGONSUBMITBUTTONCAPTIONWebElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
+		    .className("button primary")
+		    .innerText("Sign-In").build());
+        if (labelLOGONSUBMITBUTTONCAPTIONWebElement.exists()){
+            try {
+                Reporter.reportEvent("Check Not Logged In","The PPM system prompted for credentials, exiting test", Status.Passed);
+            } catch (ReportException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        WebElement menuUserIconWebElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .id("menuUserIcon").build());
+        if (menuUserIconWebElement.exists()){
+            menuUserIconWebElement.click();
+            try {
+                Reporter.reportEvent("Check Not Logged In","The PPM system auto logged in, logging off", Status.Passed);
+            } catch (ReportException e) {
+                e.printStackTrace();
+            }
+            Link signOutAdminUserLink = browser.describe(Link.class, new LinkDescription.Builder()
+                    .innerText(new RegExpProperty("Sign Out .*")).build());
+            signOutAdminUserLink.click();
+            browser.sync();
+            return;
+        }
+        else{
+            try {
+                Reporter.reportEvent("Check Not Logged In","Unexpected page came up, aborting", Status.Failed);
+            } catch (ReportException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
     }
 
 }
